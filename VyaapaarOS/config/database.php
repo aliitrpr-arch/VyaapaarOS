34ed1035f8800ac87a734c0e64b7eb1e43316464
aliitrpr-arch <aliit.rpr@gmail.com>
2026-08-13 13:17:30 +0530

    Update database.php

--- VyaapaarOS/config/database.php
+++ VyaapaarOS/config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'host' => '127.0.0.1',
-    'port' => '5432',
-    'database' => 'vyaapaaros_db',
-    'username' => 'postgres',
-    'password' => 'Shoukat#1234'
+    'host' => '://supabase.com', // नया लाइव पूलर होस्ट
+    'port' => '6543',                                     // नया पोर्ट
+    'database' => 'postgres',                            // नया डिफ़ॉल्ट DB नाम
+    'username' => 'postgres.aeaatfmrophpbgyqcrom',        // आपका विशिष्ट यूजरनेम
+    'password' => 'Shoukat#1234'                         // आपका असली पासवर्ड जो आपने सेट किया था
 ];
